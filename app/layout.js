2931723880abc623c1eca685607f81b466b75d27
Swastik Yadav <swastikb380@gmail.com>
2024-03-16 13:53:39 +0530

    initial setup and shadcn/ui installation

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,8 @@
 import { Inter } from "next/font/google";
+import { cn } from "@/lib/utils";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cn(
+        "min-h-screen bg-background font-sans antialiased",
+        inter.variable
+      )}>{children}</body>
     </html>
   );
 }
