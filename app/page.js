167f263fbe0cbc1d3656551b3831bec639ea4331
Swastik Yadav <swastikb380@gmail.com>
2024-03-17 01:52:56 +0530

    add blog page link on home page

--- app/page.js
+++ app/page.js
@@ -1,3 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <section>Hello World</section>;
+  return (
+    <>
+      <section>My NextJs Minimal Blog Boilerplate.</section>
+      <p>Ready to go with minimal setup. Move fast with blogs.</p>
+      <br />
+      <Link href="/blog">Blog &rarr;</Link>
+    </>
+  );
 }
