b1ce52a0f827b5863b13bf1d1351e186aee3ef8a
Swastik Yadav <swastikb380@gmail.com>
2024-03-19 17:03:47 +0530

    add basic homepage UI

--- app/page.js
+++ app/page.js
@@ -1,12 +1,23 @@
 import Link from "next/link";
 
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <>
-      <section>My NextJs Minimal Blog Boilerplate.</section>
-      <p>Ready to go with minimal setup. Move fast with blogs.</p>
-      <br />
-      <Link href="/blog">Blog &rarr;</Link>
-    </>
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <h1 className="text-2xl font-bold">Blog Fast!</h1>
+      <p>Start your blog in <span className="text-purple-800 font-bold dark:text-purple-500">few hours not few days.</span></p>
+      <small className="my-4">
+        Ready to go, <span className="text-purple-800 font-bold dark:text-purple-500">done for you NextJs blog setup</span>.
+        Clone, write your first blog, and deploy. As simple as that.
+        <br /><br />
+        Focus on writing, not on setting up the blog.
+        <br /><br />
+        New templates launching soon.
+      </small>
+      <Link href="/blog">
+        <Button>Read blogs</Button>
+      </Link>
+    </main>
   );
 }
